3b7a373ecd70dcb270525c77cb12a236668b6988
Elsa Rhudye Mangoye <diamant.rhudye@gmail.com>
2020-11-29 17:02:54 +0100

    projet final

--- src/searchPhotos.js
+++ src/searchPhotos.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState} from "react";
+import { createApi } from "unsplash-js";
+
+const unsplash = createApi({
+    accessKey: process.env.REACT_APP_UNSPLASH_API_KEY
+});
 
 export default function SearchPhotos() {
+    const [query, setQuery] = useState("");
+    const [pics, setPics] = useState();
+
+    const searchPhotos = async (e) => {
+        e.preventDefault();
+        unsplash.search
+        .getPhotos({query: query})
+        .then(results => {
+            setPics(results);
+            console.log(results);
+        });
+        console.log("Submitting the Form");
+    };
+    
     return (
         <>
-            <form className="form"> 
+            <form className="form" onSubmit={searchPhotos}> 
                 <label className="label" htmlFor="query"> 
                     {" "}
                      📷
@@ -13,11 +32,28 @@ export default function SearchPhotos() {
                     name="query"
                     className="input"
                     placeholder={`Essayez "Paris" ou "Chien"`}
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
                 />
                 <button type="submit" className="button">
                     Search
                 </button>
             </form>
+            <div className= "card-list">
+            {
+                pics && pics.response.results.map((pic) =>
+                    <dic className="card" key={pic.id}>
+                        <img
+                            className="card--image"
+                            alt={pic.alt_description}
+                            src={pic.urls.small}
+                            width= "50%"
+                            height= "50%">
+                        </img>
+                    </dic>
+                )
+            }
+            </div>
         </>
     );
 }
